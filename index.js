8298fe514fee09737e60b7789ee7f0d488cb682e
Furkan <muhammedcengiz1@gmail.com>
2022-09-12 13:57:32 +0300

    Kopyalama butonları eklendi ve input kısmına bazı şartlar eklenerek çevrim kısımları korundu.

--- index.js
+++ index.js
@@ -3,18 +3,59 @@ const btnEl = document.getElementById("btn-el")
 const lengthEl = document.getElementById("length")
 const volumeEl = document.getElementById("volume")
 const massEl = document.getElementById("mass")
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-*/
+const copyFeetBtn = document.getElementById("copy-feet-btn")
+const copyMeterBtn = document.getElementById("copy-meter-btn")
+const copyGallonBtn = document.getElementById("copy-gallon-btn")
+const copyLiterBtn = document.getElementById("copy-liter-btn")
+const copyPoundBtn = document.getElementById("copy-pounds-btn")
+const copyKiloBtn = document.getElementById("copy-kilos-btn")
 
-btnEl.addEventListener("click", (event) => {
+function Convert() {
+    
     const input = inputEl.value
-    //Length conversion
-    lengthEl.textContent = `${input} meters = ${(input*3.281).toFixed(3)} feet | ${input} feet = ${(input/3.281).toFixed(3)} meters`
+    if (input > 0) {
+        //Length conversion
+    lengthEl.innerHTML = `${input} meters = <span id="feet-conversion">${(input*3.281).toFixed(3)}</span> feet | ${input} feet = <span id="meter-conversion">${(input/3.281).toFixed(3)}</span> meters`
     //volume conversion
-    volumeEl.textContent = `${input} liters = ${(input*0.264).toFixed(3)} gallons | ${input} gallons = ${(input*4.546).toFixed(3)} liters`
+    volumeEl.innerHTML = `${input} liters = <span id="gallon-conversion">${(input*0.264).toFixed(3)}</span> gallons | ${input} gallons = <span id="liter-conversion">${(input*3.785).toFixed(3)}</span> liters`
     //mass conversion
-    massEl.textContent = `${input} kilos = ${(input*2.204).toFixed(3)} pounds | ${input} pounds = ${(input/2.204).toFixed(3)} kilos`
+    massEl.innerHTML = `${input} kilos = <span id="pound-conversion">${(input*2.204).toFixed(3)}</span> pounds | ${input} pounds = <span id="kilo-conversion">${(input/2.204).toFixed(3)}</span> kilos`
+    }
+    
+    
+}
+Convert()
+
+copyFeetBtn.addEventListener("click", function() {
+    let copyText = document.getElementById("feet-conversion").innerText
+    navigator.clipboard.writeText(copyText)
+})
+
+copyMeterBtn.addEventListener("click", function() {
+    let copyText = document.getElementById("meter-conversion").innerText
+    navigator.clipboard.writeText(copyText)
+})
+copyGallonBtn.addEventListener("click", function() {
+    let copyText = document.getElementById("gallon-conversion").innerText
+    navigator.clipboard.writeText(copyText)
+})
+copyLiterBtn.addEventListener("click", function() {
+    let copyText = document.getElementById("liter-conversion").innerText
+    navigator.clipboard.writeText(copyText)
+})
+copyPoundBtn.addEventListener("click", function() {
+    let copyText = document.getElementById("pound-conversion").innerText
+    navigator.clipboard.writeText(copyText)
+})
+copyKiloBtn.addEventListener("click", function() {
+    let copyText = document.getElementById("kilo-conversion").innerText
+    navigator.clipboard.writeText(copyText)
+})
+
+inputEl.addEventListener("click", (event) => {
+    if (inputEl.value == 1) { // sadece input kısmında 1 varken input kısmı silinmesi için bunu yapmalıyız yoksa her tıklamada inputu boşaltıyor
+        inputEl.value = null;
+    } else {
+
+    }
 })
